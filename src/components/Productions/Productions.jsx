37ba2f55a07edbad165dc8a3b572d3920af2d554
Vitali Hoila <vitali.hoila@coverwallet.com>
2024-01-15 14:20:03 +0300

    modified products pages

--- src/components/Productions/Productions.jsx
+++ src/components/Productions/Productions.jsx
@@ -2,19 +2,23 @@ import s from './Productions.module.css';
 
 function Productions() {
   return (
-    <div className={s.main}>
+    <div>
       <main className="container-fluid">
         <div className="p-2 p-md-1 mb-2 rounded text-body-emphasis bg-body-secondary">
           <div className="container-fluid py-5" id='top'>
             <h1 className="display-4 fw-bold fst-italic text-center" >Мы готовы предложить</h1>
           </div>
         </div>
-        <div className="album py-5 bg-body-tertiary" >
+        <div className="album py-5 center" >
           <div className="container">
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-2">
               <div className="col">
                 <div className="card shadow-sm">
-                  <img className="bd-placeholder-img card-img-top" width="100%" height="430" src='https://st9.stpulscen.ru/images/product/141/102/369_original.jpg' />
+                  <div className="card-body">
+                    <div className={s.center}>
+                      <img width="70%" src='https://st9.stpulscen.ru/images/product/141/102/369_original.jpg' />
+                    </div>
+                  </div>
                   <div className="card-body">
                     <p className="card-text"><ul className="fs-4">
                       <h2>Дрова березовые</h2>
@@ -30,7 +34,11 @@ function Productions() {
 
               <div className="col">
                 <div className="card shadow-sm">
-                  <img className="bd-placeholder-img card-img-top" width="100%" height="431" src='https://m-files.cdnvideo.ru/lpfile/f/5/2/f525e547b3723e36e31c0641b9da57e5/-/resize/600/f.jpeg' />
+                  <div className="card-body">
+                    <div className={s.center}>
+                      <img width="79%" src='https://m-files.cdnvideo.ru/lpfile/f/5/2/f525e547b3723e36e31c0641b9da57e5/-/resize/600/f.jpeg' />
+                    </div>
+                  </div>
                   <div className="card-body">
                     <p className="card-text"><ul className="fs-4">
                       <h2>Дрова микс</h2>
@@ -48,7 +56,11 @@ function Productions() {
               </div>
               <div className="col">
                 <div className="card shadow-sm">
-                  <img className="bd-placeholder-img card-img-top" width="100%" height="425" src='https://m-files.cdnvideo.ru/lpfile/f/f/4/ff4294176708554557df5cf905d7b596/-/resize/600/f.jpeg' />
+                  <div className="card-body">
+                    <div className={s.center}>
+                      <img width="80%" src='https://m-files.cdnvideo.ru/lpfile/f/f/4/ff4294176708554557df5cf905d7b596/-/resize/600/f.jpeg' />
+                    </div>
+                  </div>
                   <div className="card-body">
                     <p className="card-text"><ul className="fs-4">
                       <h2>Дрова ольховые</h2>
@@ -63,8 +75,12 @@ function Productions() {
                 </div>
               </div>
               <div className="col">
-                <div className="card shadow">
-                  <img className="bd-placeholder-img card-img-top" width="100%" height="425" src='https://m-files.cdnvideo.ru/lpfile/c/0/8/c086245363520a5cc307098eb2072607/-/resize/600/f.jpeg' />
+                <div className="card shadow-sm">
+                  <div className="card-body">
+                    <div className={s.center}>
+                      <img width='80%' src='https://m-files.cdnvideo.ru/lpfile/c/0/8/c086245363520a5cc307098eb2072607/-/resize/600/f.jpeg' />
+                    </div>
+                  </div>
                   <div className="card-body">
                     <p className="card-text"><ul className="fs-4">
                       <p className='text-center h2'>Дрова хвойных пород (сухие? *)</p>
